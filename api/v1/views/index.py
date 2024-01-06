214be7dc6ba044bc69463a48dc7780fd28b24e29
Brian Kimathi M <briankimathi94@gmail.com>
2024-01-07 00:23:54 +0300

    print count of all classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,32 @@
 """returns a JSON: status OK"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.user import User
+from models.review import Review
+
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
+}
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """returns a JSON: status OK"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    dic = {key: storage.count(value) for key, value in classes.items()}
+    return jsonify(dic)
